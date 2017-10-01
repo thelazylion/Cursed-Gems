a4f38f1cc879bb313b60f6b8e44a2a91a5c6be75
thelazylion <tworad@gmail.com>
2017-09-30 21:14:57 -0700

    Resetting git

--- Intermediate/Build/Win64/TheCursedGems/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
+++
@@ -1,1 +0,0 @@
-#include "E:/Cloud/Dropbox/Private/Files/School/Rapid Game Prototype/Elements/TheCursedGems/Intermediate/Build/Win64/TheCursedGems/Development/UELinkerFixups/PCH.UELinkerFixups.h"
--- Intermediate/Build/Win64/TheCursedGems/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for E:\Cloud\Dropbox\Private\Files\School\Rapid Game Prototype\Elements\TheCursedGems\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
-#include "E:\Cloud\Dropbox\Private\Files\School\Rapid Game Prototype\Elements\TheCursedGems\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
